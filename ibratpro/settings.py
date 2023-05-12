598fcd5213cc8795502fc8979fd6a4d1caa98e29
Muhammadali <132582994+Muhameddev@users.noreply.github.com>
2023-05-13 00:40:53 +0500

    Update settings.py

--- ibratpro/settings.py
+++ ibratpro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))$xxiqq%eup6=90ysohz=da^!xfo%p4rys3gdjdj-vtvw1r6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
