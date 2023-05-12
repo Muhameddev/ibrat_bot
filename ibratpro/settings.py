552d2d0fa2517cdf2930ef3a898673a721adda1b
Muhameddev <132582994+Muhameddev@users.noreply.github.com>
2023-05-12 20:30:55 +0500

    Ibrat bot backendi

--- ibratpro/settings.py
+++ ibratpro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))$xxiqq%eup6=90ysohz=da^!xfo%p4rys3gdjdj-vtvw1r6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static')),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- ibratpro/urls.py
+++ ibratpro/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from ibratapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin-ibrat/', admin.site.urls),
     path('nom/', NomView.as_view(), name="nom"),
     path('video/', VideoView.as_view(), name="video"),
 ]
+urlpatterns+= static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
